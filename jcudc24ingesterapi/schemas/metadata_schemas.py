9ff2e687286191116600ba6b9104e32fcd1b4c63
Casey Bajema <casey@bajtech.com.au>
2012-12-14 13:58:31 +1000

    Updated some comments

--- jcudc24ingesterapi/schemas/metadata_schemas.py
+++ jcudc24ingesterapi/schemas/metadata_schemas.py
@@ -7,9 +7,9 @@ __author__ = 'Casey Bajema'
 
 class MetadataSchema(dict):
     """
-    Base class for all calibration schemas that provide a known type.
+    Base class for all calibration models that provide a known type.
 
-    All calibration schemas that will be used need to be setup when creating the dataset.
+    All calibration models that will be used need to be setup when creating the dataset.
 
     Each calibration schema has a 1:1 relationship with each data_entry.  This means that there can only
     be 1 QualitySchema calibration for a specific data_entry but there may be a different calibration
