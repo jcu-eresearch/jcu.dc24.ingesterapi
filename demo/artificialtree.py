ffd37dc14994a6a7334024702488796483848200
Nigel Sim <nigel.sim@gmail.com>
2013-01-28 14:57:34 +1000

    added artificial tree

---
+++ demo/artificialtree.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+from jcudc24ingesterapi.models.dataset import Dataset
+from jcudc24ingesterapi.models.locations import Location, Region
+from jcudc24ingesterapi.schemas.data_types import FileDataType, Double, String
+from jcudc24ingesterapi.models.data_sources import PullDataSource, DatasetDataSource
+from jcudc24ingesterapi.models.data_entry import DataEntry
+from jcudc24ingesterapi.ingester_platform_api import IngesterPlatformAPI
+from jcudc24ingesterapi.authentication import CredentialsAuthentication
+from jcudc24ingesterapi.models.metadata import MetadataEntry
+from jcudc24ingesterapi.schemas.metadata_schemas import DataEntryMetadataSchema, DatasetMetadataSchema
+from jcudc24ingesterapi.models.sampling import RepeatSampling, PeriodicSampling, CustomSampling
+from jcudc24ingesterapi.ingester_exceptions import UnsupportedSchemaError, InvalidObjectError, UnknownObjectError, AuthenticationError
+from jcudc24ingesterapi.schemas.data_entry_schemas import DataEntrySchema
+
+auth = CredentialsAuthentication("casey", "password")
+ingester_platform = IngesterPlatformAPI("http://localhost:8080/api", auth)
+
+# Setup the file storage schema
+file_schema = DataEntrySchema("arduino_file")
+file_schema.addAttr(FileDataType("file"))
+file_schema = ingester_platform.post(file_schema)
+
+temp_schema = DataEntrySchema("temperature_reading")
+temp_schema.addAttr(Double("temp"))
+temp_schema = ingester_platform.post(temp_schema)
+
+# Setup the location
+loc = Location(-19.34427, 146.784197, "Mt Stuart", 100, None)
+loc = ingester_platform.post(loc)
+
+# Create the dataset to store the data in
+file_dataset = Dataset(loc.id, file_schema.id, PullDataSource("http://emu.hpc.jcu.edu.au/tree/split/", field="file", recursive=True),
+                PeriodicSampling(10000))
+file_dataset.enabled = False
+file_dataset = ingester_platform.post(file_dataset)
+
+# This processing script is 
+processing_script = """
+import os
+import datetime
+from dc24_ingester_platform.utils import *
+
+def process(cwd, data_entry):
+    data_entry = data_entry[0]
+    ret = []
+    started = False
+    with open(os.path.join(cwd, data_entry["file"]["path"])) as f:
+        for l in f.readlines():
+            l = l.strip()
+            if not started:
+                if l == "BEGIN TEMP": started = True
+            elif started and l == "END TEMP":
+                break
+            else:
+                # parse line
+                l = l.split(",")
+                if l[1] != "%s": continue
+                ret.append( {"timestamp":format_timestamp(datetime.datetime.now()), "temp":float(l[2]) } )
+    return ret
+"""
+
+temp_dataset1 = Dataset(loc.id, file_schema.id, DatasetDataSource(file_dataset.id, processing_script=processing_script%"28180E08030000BE"))
+temp_dataset1.enabled = True
+temp_dataset1 = ingester_platform.post(temp_dataset1)
+
+ingester_platform.enableDataset(file_dataset.id)
+
